80f64e00a111d5fd784fed818d574910d798a6da
Jonathan Gamble <jdgamble555@gmail.com>
2021-12-26 14:51:49 -0600

    go

--- server.ts
+++ server.ts
@@ -9,39 +9,42 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
-  const server = express();
-  const distFolder = join(process.cwd(), 'api/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+//export function app(): express.Express {
+const server = express();
+const distFolder = join(process.cwd(), 'api/browser');
+const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+server.engine('html', ngExpressEngine({
+  bootstrap: AppServerModule,
+}));
 
-  server.set('view engine', 'html');
-  server.set('views', distFolder);
+server.set('view engine', 'html');
+server.set('views', distFolder);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+// Example Express Rest API endpoints
+// server.get('/api/**', (req, res) => { });
+// Serve static files from /browser
+server.get('*.*', express.static(distFolder, {
+  maxAge: '1y'
+}));
 
-  // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
-  });
+// All regular routes use the Universal engine
+server.get('*', (req, res) => {
+  res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+});
 
-  return server;
-}
+const app = server;
+module.exports = app;
+
+//  return server;
+//}
 
 function run(): void {
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
-  const server = app();
+  const server = app;
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
