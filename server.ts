412d44d2619583650e97334b9583de2adf69d949
Jonathan Gamble <jdgamble555@gmail.com>
2021-12-26 16:58:57 -0600

    s

--- server.ts
+++ server.ts
@@ -9,37 +9,39 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export default function app(): express.Express {
-  const server = express();
-  const distFolder = join(process.cwd(), 'dist/test/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+//export default function app(): express.Express {
+const server = express();
+const distFolder = join(process.cwd(), 'dist/test/browser');
+const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+server.engine('html', ngExpressEngine({
+  bootstrap: AppServerModule,
+}));
 
-  server.set('view engine', 'html');
-  server.set('views', distFolder);
+server.set('view engine', 'html');
+server.set('views', distFolder);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+// Example Express Rest API endpoints
+// server.get('/api/**', (req, res) => { });
+// Serve static files from /browser
+server.get('*.*', express.static(distFolder, {
+  maxAge: '1y'
+}));
 
-  // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
-  });
+// All regular routes use the Universal engine
+server.get('*', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+});
 
-  return server;
-}
+//return server;
+//}
 
-function run(): void {
+module.exports = server;
+
+/*function run(): void {
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
@@ -47,16 +49,16 @@ function run(): void {
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
-}
+}*/
 
 // Webpack will replace 'require' with '__webpack_require__'
 // '__non_webpack_require__' is a proxy to Node 'require'
 // The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
+/*declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
-}
+}*/
 
-export * from './src/main.server';
+//export * from './src/main.server';
